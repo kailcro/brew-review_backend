df150e96bf74455a41746e949855f9c11ab77db6
kailcro <kailey.crothers@uconn.edu>
2020-06-29 16:48:26 -0400

    Delete a review

--- curl-scripts/beers/reviews/destroy.sh
+++ curl-scripts/beers/reviews/destroy.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 API="http://localhost:4741"
-URL_PATH="/examples"
+URL_PATH="/reviews"
 
 curl "${API}${URL_PATH}/${ID}" \
   --include \
   --request DELETE \
-  --header "Authorization: Bearer ${TOKEN}"
+  --header "Authorization: Bearer ${TOKEN}" \
+  --data '{
+      "review": {
+        "beerId": "'"${BEER_ID}"'"
+      }
+    }'
 
 echo
